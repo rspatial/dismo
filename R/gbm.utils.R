461663b79af9445a6ed26d2df4f9ae675c39f849
areichmuth <Anne_Reichmuth@web.de>
2023-04-14 14:14:32 +0200

    Change to continuous input

--- R/gbm.utils.R
+++ R/gbm.utils.R
@@ -22,8 +22,8 @@
         stop("obs and preds must be equal lengths")
 	}
     n.x <- length(obsdat[obsdat == 0])
-    n.y <- length(obsdat[obsdat == 1])
-    xy <- c(preddat[obsdat == 0], preddat[obsdat == 1])
+    n.y <- length(obsdat[obsdat > 0])
+    xy <- c(preddat[obsdat == 0], preddat[obsdat > 0])
     rnk <- rank(xy)
     wilc <- ((n.x * n.y) + ((n.x * (n.x + 1))/2) - sum(rnk[1:n.x]))/(n.x * n.y)
     return(round(wilc, 4))
